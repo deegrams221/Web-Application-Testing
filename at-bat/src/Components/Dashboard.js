0d69c42b22a6d5fde00e0fed7a1188e2bd6ab520
Diana Grams <deegrams221@gmail.com>
2019-09-05 15:43:07 -0600

    added in strikes and fouls

--- at-bat/src/Components/Dashboard.js
+++ at-bat/src/Components/Dashboard.js
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Display from './Display';
+
+export const addBalls = balls => {
+  return balls + 1;
+}
+
+export const addStrikes = strikes => {
+  return strikes + 1;
+}
 
 export default function Dashboard () {
   const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
 
   const ballsIncrement = () => {
     if(balls < 4){
-      setBalls(balls + 1);
+      setBalls(addBalls(balls));
     } else {
-        setBalls(0)
+      setBalls(0);
+    }
+  }
+
+  const strikesIncrement = () => {
+    if(strikes < 3){
+      setStrikes(addStrikes(strikes));
+    } else {
+      setStrikes(0);
+    }
+  }
+
+  const foulIncrement = () => {
+    if(strikes === 0){
+      setStrikes(1);
+    } else if (strikes === 1) {
+      setStrikes(2);
+    } else {
+      setStrikes(strikes);
     }
   }
 
   return(
     <div>
-      <h1>Dashboard</h1>
-        <Display balls={balls} setBalls={setBalls}/>
+      <h1>Baseball App</h1>
+        <Display balls={balls} setBalls={setBalls} strikes={strikes} setStrikes={setStrikes} />
         <button onClick={() => ballsIncrement()}>Ball</button>
-        <button>Strike</button>
-        <button>Foul</button>
+        <button onClick={() => strikesIncrement()}>Strike</button>
+        <button onClick={() => foulIncrement()}>Foul</button>
         <button>Hit</button>
     </div>
   );
--- at-bat/src/Components/Display.js
+++ at-bat/src/Components/Display.js
@@ -4,6 +4,7 @@ export default function Display (props){
   return(
     <div>
       <h2>Balls: {props.balls}</h2>
+      <h2>Strikes: {props.strikes}</h2>
     </div>
   );
 } 
